396138ef458782e00cb48f59b43fb7e2865dbda2
Elkarroudi <amineelkarroudi04@proton.me>
2025-03-17 14:25:29 +0000

    feat: add custom exception

---
+++ src/main/java/com/inventivIt/dreamCase/util/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.inventivIt.dreamCase.util.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
+}
