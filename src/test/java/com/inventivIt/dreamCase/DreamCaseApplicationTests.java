68312d5001897d11f4f763414c6d576eff013c5e
Elkarroudi <amineelkarroudi04@proton.me>
2025-03-17 15:25:55 +0000

    test: add unit tests for mapper

---
+++ src/test/java/com/inventivIt/dreamCase/DreamCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventivIt.dreamCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
